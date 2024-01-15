1441db823f0d4f0d80902b9a0497069803b7d2a9
Stephany <128330097+sstephanyy@users.noreply.github.com>
2024-01-15 17:36:47 -0300

    Update ConnectionContext.cs

--- TreinandoAPI/Infra/ConnectionContext.cs
+++ TreinandoAPI/Infra/ConnectionContext.cs
@@ -9,10 +9,7 @@ namespace TreinandoAPI.Infra
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql(
-                "Server=  ec2-35-169-88-97.compute-1.amazonaws.com;" +
-                "Port=5432;Database=dev_stephany;" +
-                "User Id= stephany;" +
-                "Password=TKFBF0OdWFoYgqr6hy45;");
+               );
 
 
     }
